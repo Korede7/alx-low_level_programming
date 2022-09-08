07c45b67389602efc9a7541e04b0fb8376e7dcb8
Korede7 <korededuyile7@.com>
2022-09-08 15:51:39 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("size of a char: %lu byte(s)\n", sizeof(d));
 	printf("size of an int: %lu byte(s)\n", sizeof(a));
 	printf("size of a long int: %lu byte(s)\n", sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(c));
 	printf("size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
