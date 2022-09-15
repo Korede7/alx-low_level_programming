a253d623e2f0b5d418081d5eeebb074a994a0fc2
Korede7 <korededuyile7@.com>
2022-09-15 10:25:50 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,14 +15,14 @@ void print_diagonal(int n)
 	{
 		for (len = 0; len < n; len++)
 		{
-
 			for (space = 0; space < len; space++)
 			{
 				putchar(' ');
 			}
+
 			putchar('\\');
-			
-			if (len == (n - 1)
+
+			if (len == (n - 1))
 			{
 				continue;
 			}
